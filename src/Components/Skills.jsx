6ee75fd016c2e18c9aa491bdfa0343cdbdb97b71
tushal024 <tushalshekh@gmail.com>
2025-03-08 10:10:51 +0530

    Check

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import js from '../assets/js.png'
-import css from '../assets/css-3.png'
-import html from '../assets/html-5.png'
-import react from '../assets/react.svg'
-import tailwind from '../assets/tailwind.png'
-import git from '../assets/git.png'
-import chakra from '../assets/chakra ui.png'
-import reduxlogo from '../assets/redux.png'
-import boot from '../assets/boot.png'
+// import js from '../assets/js.png'
+// import css from '../assets/css-3.png'
+// import html from '../assets/html-5.png'
+// import react from '../assets/react.svg'
+// import tailwind from '../assets/tailwind.png'
+// import git from '../assets/git.png'
+// import chakra from '../assets/chakra ui.png'
+// import reduxlogo from '../assets/redux.png'
+// import boot from '../assets/boot.png'
 
 
 
@@ -16,7 +16,7 @@ const Skills = () => {
     <div id='skills' className='w-[90%]  m-[auto] gap-x-[80px] md:grid-cols-3  xl:grid-cols-5    grid-cols-1    sm:grid-cols-2  gap-y-[20px] grid  h-[auto]  mt-[50px]  ' >
 
       <div id="t1" className=' flex-col    flex justify-center items-center   h-[200px] border  rounded-[10px]  text-[white] ' >
-        <img src={js} alt="" className='w-[100px] rounded-[10px] ' />
+        {/* <img src={js} alt="" className='w-[100px] rounded-[10px] ' /> */}
 
         <h3 className='mt-[20px] ' >
           JavaScript
@@ -24,7 +24,7 @@ const Skills = () => {
 
       </div>
       <div id="t1" className='  flex-col    h-[200px] border  flex justify-center items-center rounded-[10px]  text-[white] ' >
-        <img src={html} alt="" className='w-[100px] rounded-[10px]   ' />
+        {/* <img src={html} alt="" className='w-[100px] rounded-[10px]   ' /> */}
         <h3 className='mt-[20px] ' >
           HTML
         </h3>
@@ -33,7 +33,7 @@ const Skills = () => {
       </div>
       <div id="t1" className='   flex-col    h-[200px] border  flex justify-center items-center  rounded-[10px]  text-[white] ' >
 
-        <img src={css} alt="" className='w-[100px] rounded-[10px]   ' />
+        {/* <img src={css} alt="" className='w-[100px] rounded-[10px]   ' /> */}
         <h3 className='mt-[20px] ' >
           CSS
         </h3>
@@ -43,7 +43,7 @@ const Skills = () => {
       <div id="t1" className='   flex-col   h-[200px] border   flex justify-center items-center  rounded-[10px]  text-[white] ' >
 
 
-        <img src={react} alt="" className='w-[100px] rounded-[10px]   ' />
+        {/* <img src={react} alt="" className='w-[100px] rounded-[10px]   ' /> */}
 
         <h3 className='mt-[20px] ' >
           React
@@ -54,7 +54,7 @@ const Skills = () => {
       <div id="t1" className='   flex-col    h-[200px] border   flex justify-center items-center  rounded-[10px]  text-[white] ' >
 
         {/* Redux */}
-        <img src={reduxlogo} alt="" className='w-[100px] rounded-[10px]   ' />
+        {/* <img src={reduxlogo} alt="" className='w-[100px] rounded-[10px]   ' /> */}
 
         <h3 className='mt-[20px] ' >
           Redux
@@ -64,7 +64,7 @@ const Skills = () => {
       </div>
       <div id="t1" className='   flex-col    h-[200px] border   flex justify-center items-center  rounded-[10px]  text-[white] ' >
 
-        <img src={tailwind} alt="" className='w-[100px] rounded-[10px]   ' />
+        {/* <img src={tailwind} alt="" className='w-[100px] rounded-[10px]   ' /> */}
         <h3 className='mt-[20px] ' >
           Tailwind
         </h3>
@@ -75,7 +75,7 @@ const Skills = () => {
       <div id="t1" className='   flex-col   h-[200px] border   flex justify-center items-center  rounded-[10px]  text-[white] ' >
 
         {/* Boostrap */}
-        <img src={boot} className='w-[100px] rounded-[10px]   ' alt="" />
+        {/* <img src={boot} className='w-[100px] rounded-[10px]   ' alt="" /> */}
 
         <h3 className='mt-[20px] ' >
           Boostrap
@@ -85,7 +85,7 @@ const Skills = () => {
       </div>
       <div id="t1" className='   flex-col    h-[200px] border   flex justify-center items-center rounded-[10px]  text-[white] ' >
 
-        <img src={git} className='w-[100px] rounded-[10px]   ' alt="" />
+        {/* <img src={git} className='w-[100px] rounded-[10px]   ' alt="" /> */}
         <h3 className='mt-[20px] ' >
           Git
         </h3>
@@ -94,7 +94,7 @@ const Skills = () => {
       </div>
       <div id="t1" className='   flex-col   h-[200px] border   flex justify-center items-center rounded-[10px]  text-[white] ' >
 
-        <img src={chakra} className='w-[100px] rounded-[10px]   ' alt="" />
+        {/* <img src={chakra} className='w-[100px] rounded-[10px]   ' alt="" /> */}
         <h3 className='mt-[20px] ' >
           Chakra UI
         </h3>
@@ -103,7 +103,7 @@ const Skills = () => {
       </div>
       <div id="t1" className='   flex-col    h-[200px] border   flex justify-center items-center rounded-[10px]  text-[white] ' >
 
-        <img src={git} className='w-[100px] rounded-[10px]   ' alt="" />
+        {/* <img src={git} className='w-[100px] rounded-[10px]   ' alt="" /> */}
         <h3 className='mt-[20px] ' >
           Git
         </h3>
@@ -112,7 +112,7 @@ const Skills = () => {
       </div>
       <div id="t1" className='   flex-col   h-[200px] border   flex justify-center items-center rounded-[10px]  text-[white] ' >
 
-        <img src={chakra} className='w-[100px] rounded-[10px]   ' alt="" />
+        {/* <img src={chakra} className='w-[100px] rounded-[10px]   ' alt="" /> */}
         <h3 className='mt-[20px] ' >
           Chakra UI
         </h3>
